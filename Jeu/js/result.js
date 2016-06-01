f267f790cab3ab4e6555985e6cb6576aecf3c12b
qll1994 <helodieqll@gmail.com>
2016-06-01 13:55:53 +0200

    Update result.js

--- Jeu/js/result.js
+++ Jeu/js/result.js
@@ -2,20 +2,16 @@ var resultObj = function()
 {
 	this.vie;
 	this.time;
-	this.fini;
-	this.niv = [];
+	//this.fini;
+	this.niv;
 
 }
 resultObj.prototype.init = function()
 {
 	this.vie = 3;
 	this.time = 0;
-	this.fini = 0;
-	for(var i = 1;i<4;i++)
-	{
-		this.niv[i] = 0;
-	}
-	this.niv[1] = 1;//niveau1
+	//this.fini = 0;
+	this.niv = 1;
 
 }
 resultObj.prototype.draw = function()
@@ -25,6 +21,7 @@ resultObj.prototype.draw = function()
 	    ctx.font = "30px Arial";
 	    ctx.fillText("Vie:" + this.vie,canWidth/2,40);
 	    ctx.fillText("Temps:" + this.time + "s",canWidth/2,80);
+	    ctx.fillText("Niveau:" + this.niv,canWidth/2,120);
 	    ctx.fillStyle = "white";
 	    ctx.textAlign = "center";			
 	}
@@ -41,8 +38,19 @@ resultObj.prototype.update = function()
 	}else
 	{
 		this.vie = 0;
-		//alert("Ce jeu est fini!Vous avec reussi d'insister "+ this.time + "s!");
+		//alert("Ce jeu est fini!Vous avez reussi d'insister "+ this.time + "s!");
 	}
+	if(this.time > 10 && this.time < 20)
+	{
+		this.niv = 2;
+	}else if(this.time >= 20 && this.time <= 30)
+	{
+		this.niv = 3;
+	}else if(this.time >30)
+	{
+		this.niv = 4;
+	}
+
 /*	if(this.time > 40 && this.time < 50)
 	{
 		//ctx.fillText("Niveau update...! Niveau 1!",canWidth/2,canHeight/2);
